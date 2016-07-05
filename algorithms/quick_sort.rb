2bf20a642a17340da513d72e770b4bf404e21b49
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-04 21:41:31 -0300

    Add Sorting Algorithms

---
+++ algorithms/quick_sort.rb
@@ -0,0 +1,12 @@
+#use metaprogramming to open up Array class and implement quick sort from inside
+class Array
+  def quicksort
+    return [] if empty?
+    pivot = delete_at(rand(size))
+    left, right = partition(&pivot.method(:>))
+    return *left.quicksort, pivot, *right.quicksort
+  end
+end
+
+arr = [34,2,1,5,3]
+p arr.quicksort
